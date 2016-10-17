e727c41b1746058da7c6689f7862bc7e4e75694c
Joonas Westlin <joonas.westlin@onsight.fi>
2016-10-17 21:31:12 +0300

    Added all the other action methods to the ProductsController

--- src/ElectronicsStoreApi/Controllers/ProductsController.cs
+++ src/ElectronicsStoreApi/Controllers/ProductsController.cs
@@ -24,5 +24,56 @@ namespace ElectronicsStoreApi.Controllers
             List<Product> products = _products.GetAllProducts();
             return Ok(products);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(long id)
+        {
+            Product product = _products.GetProduct(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Product createdProduct = _products.CreateProduct(product);
+
+            return CreatedAtAction(
+                nameof(GetProduct), new { id = createdProduct.Id }, createdProduct);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(long id, [FromBody] Product product)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                _products.UpdateProduct(id, product);
+                return Ok();
+            }
+            catch (EntityNotFoundException<Product>)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(long id)
+        {
+            _products.DeleteProduct(id);
+            return Ok();
+        }
     }
 }
