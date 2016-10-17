c0bb124333c52c2365e013b1c7cbc8ff1f36c13e
Joonas Westlin <joonas.westlin@onsight.fi>
2016-10-17 21:06:45 +0300

    Created basic ProductsController

---
+++ src/ElectronicsStoreApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using ElectronicsStoreApi.DomainModels;
+using ElectronicsStoreApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicsStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _products;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _products = productRepository;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAllProducts()
+        {
+            List<Product> products = _products.GetAllProducts();
+            return Ok(products);
+        }
+    }
+}
